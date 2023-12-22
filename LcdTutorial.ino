7db4856cb1ecf69879340f143f6c3a8f3cac4235
AbdullahBamufleh <148006077+AbdullahBamufleh@users.noreply.github.com>
2023-12-22 11:44:08 +0300

    Update LcdTutorial.ino
    more comments

--- LcdTutorial.ino
+++ LcdTutorial.ino
@@ -1,8 +1,8 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> // import LCD library
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // RS, E, D4, D5, D6,
 
-byte c1[] = {
+byte c1[] = { // حرف الغين غ
 B00100,
 B00000, 
 B00111, 
@@ -13,7 +13,7 @@ B00000,
 B00000
 };
 
-byte c2[] = {
+byte c2[] = { // حرف الزين ز
 B00000,
 B01000, 
 B00000, 
@@ -23,7 +23,7 @@ B01001,
 B01000, 
 B11000
 };
-byte c3[] = {
+byte c3[] = { // حرف التاء ة
 B00110, 
 B00000, 
 B11000, 
@@ -34,19 +34,19 @@ B01111,
 B00000
 };
 void setup() {
-  lcd.begin(16,2);
+  lcd.begin(16,2); // setup the LCD boared with corrcet dimensions
 
-  lcd.createChar(1,c1);
-  lcd.setCursor(15,0); 
-  lcd.write(1);
+  lcd.createChar(1,c1); // make a new character with index 1, and equal to c1
+  lcd.setCursor(15,0);  // set the cursor where to start writing
+  lcd.write(1); // display the character with index 1
 
-  lcd.createChar(2,c2);
-  lcd.setCursor(14,0);
-  lcd.write(2);
+  lcd.createChar(2,c2); // make a new character with index 2, and equal to c2
+  lcd.setCursor(14,0); // set the cursor where to start writing
+  lcd.write(2); // display the character with index 2
 
-  lcd.createChar(3,c3); 
-  lcd.setCursor(13,0) ;
-  lcd.write(3);
+  lcd.createChar(3,c3); // make a new character with index 3, and equal to c3
+  lcd.setCursor(13,0) ; // set the cursor where to start writing
+  lcd.write(3); // display the character with index 3
 
 }
 
